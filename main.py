a8fbf0017ce34ec20d13c707196ef68b9bb74f32
Sanket Bodake <bodake871@gmail.com>
2023-05-06 20:14:28 +0530

    template anme agin change

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from flask import Flask,render_template,request
 import pickle
 
-app=Flask(__name__,template_folder="Templates")
+app=Flask(__name__,template_folder="templates\.")
 
 count_vec=pickle.load(open("countvector.pkl","rb"))
 model=pickle.load(open("Langmodel.pkl","rb"))
